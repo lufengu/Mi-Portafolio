734f1f069133725ca4d1783b0ea0c3c84a23361c
lufengu <luisafer.ariasdelat@gmail.com>
2024-05-03 03:04:12 -0500

    Updated

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const typed = new Typed('.typed', {
+    strings: ['Ingenieria informatica...','Programación...', 'Lufengu...', 'Pixel-art...', 'Deportista...'],
+    typeSpeed:45,
+    backSpeed:60,
+    loop: true
+});
---
+++ js/moda.js
@@ -0,0 +1,25 @@
+// Obtener todas las imágenes del portafolio
+const imagenesPortafolio = document.getElementsByClassName("imagen-port");
+
+// Agregar un evento clic a cada imagen
+for (let i = 0; i < imagenesPortafolio.length; i++) {
+    imagenesPortafolio[i].addEventListener("click", () => {
+        // Obtener la imagen específica que se hizo clic
+        const imagen = imagenesPortafolio[i].querySelector("img");
+        
+        // Obtener el modal y la imagen dentro del modal
+        const modal = document.getElementById("modal-img");
+        const imagenModal = modal.querySelector("img");
+        
+        // Asignar la imagen específica al modal
+        imagenModal.src = imagen.src;
+        
+        // Mostrar el modal ayudaaaaaaaaaaa :''V
+        modal.style.display = "block";
+    });
+}
+
+// Agregar evento clic al área de pantalla del modal para cerrarlo
+document.getElementById("screen-modal").addEventListener("click", () => {
+    document.getElementById("modal-img").style.display = "none";
+});
